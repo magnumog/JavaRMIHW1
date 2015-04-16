c87d93394b618c8b90b7317511c88953c2dc5aa8
Magnus Mogstad <magnumog@kenny.cs.ucsb.edu>
2015-04-16 13:40:32 -0700

    some mod to TSP

--- src/tasks/TaskEuclideanTsp.java
+++ src/tasks/TaskEuclideanTsp.java
@@ -6,9 +6,16 @@ import api.Task;
 
 public class TaskEuclideanTsp implements Task<List<Integer>> {
 	private double[][] cities;
+	private double[][] dist;
 
 	public TaskEuclideanTsp(double[][] cities) {	
 		this.cities = cities;
+		this.dist = new double[cities.length][cities.length];
+//		for(int i=0;i<this.cities.length;i++){
+//			for(int j=0;j<this.cities.length;j++) {
+//				dist[i][j] = dist[j][i] = dist(cities[i],cities[j]);
+//			}
+//		}
 	}
 	
 	@Override
